46cb540238c6d3a68cc2610aebccd01e7517e8cd
Joses W. Ho <joseshowh@gmail.com>
2017-07-27 12:31:03 +0800

    v 0.319
    simple dependency checking added.

--- setup.py
+++ setup.py
@@ -1,12 +1,54 @@
 from setuptools import setup, find_packages
+import os
+# Taken from setup.py in seaborn.
+# temporarily redirect config directory to prevent matplotlib importing
+# testing that for writeable directory which results in sandbox error in
+# certain easy_install versions
+os.environ["MPLCONFIGDIR"]="."
 
-setup(name='bootstrap_contrast',
-      version='0.318',
-      description='Calculation and Visualization of Confidence Intervals and Effect Sizes for Python.',
-      packages = find_packages(),
-      install_requires = ['numpy','scipy','pandas','seaborn','matplotlib'],
-      url='http://github.com/josesho/bootstrap_contrast',
-      author='Joses Ho',
-      author_email='joseshowh@gmail.com',
-      license='GNU GPLv3',
-      zip_safe=False)
+# Modified from from setup.py in seaborn.
+try:
+    from setuptools import setup
+    _has_setuptools=True
+except ImportError:
+    from distutils.core import setup
+
+def check_dependencies():
+    to_install=[]
+
+    try:
+        import numpy
+    except ImportError:
+        to_install.append('numpy')
+    try:
+        import scipy
+    except ImportError:
+        to_install.append('scipy')
+    try:
+        import matplotlib
+    except ImportError:
+        to_install.append('matplotlib')
+    try:
+        import pandas
+    except ImportError:
+        to_install.append('pandas')
+    try:
+        import seaborn
+    except ImportError:
+        to_install.append('seaborn')
+
+    return to_install
+
+if __name__=="__main__":
+    setup(name='bootstrap_contrast',
+        author='Joses Ho',
+        email='joseshowh@gmail.com'
+        version='0.319',
+        description='Calculation and Visualization of Confidence Intervals and Effect Sizes for Python.',
+        packages=find_packages(),
+        install_requires=to_install,
+        url='http://github.com/josesho/bootstrap_contrast',
+        author='Joses Ho',
+        author_email='joseshowh@gmail.com',
+        license='GNU GPLv3',
+        zip_safe=False)
