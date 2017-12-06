6ba58a39b55379fe46fc3c6b9ae6e3c72ec2ff92
Joses W. Ho <joseshowh@gmail.com>
2017-12-06 17:18:58 +0800

    add gapped_lines()

--- bootstrap_contrast/plot_tools.py
+++ bootstrap_contrast/plot_tools.py
@@ -310,3 +310,98 @@ def get_swarm_spans(coll):
         return x.min(), x.max(), y.min(), y.max()
     except ValueError:
         return None
+
+def gapped_lines(data, x, y,
+                 type='mean_sd',
+                 offset=0.3,
+                 ax=None,
+                 **kwargs):
+    '''
+    Convenience function to plot the standard devations as vertical
+    errorbars. The mean is a gap defined by negative space.
+
+    This style is inspired by Edward Tufte's redesign of the boxplot.
+    See The Visual Display of Quantitative Information (1983), pp.128-130.
+
+    Keywords
+    --------
+    data: pandas DataFrame.
+        This DataFrame should be in 'long' format.
+
+    x, y: string.
+        x and y columns to be plotted.
+
+    type: ['mean_sd', 'median_quartiles',], default 'mean_sd'
+        Plots the summary statistics for each group. If 'mean_sd', then the
+        mean and standard deviation of each group is plotted as a gapped line.
+        If 'median_quantiles', then the median and 25th and 75th percentiles of
+        each group is plotted instead.
+
+    offset: float, default 0.3
+        The x-offset of the mean-sd line.
+
+    ax: matplotlib Axes object, default None
+        If a matplotlib Axes object is specified, the gapped lines will be
+        plotted in order on this axes. If None, the current axes (plt.gca())
+        is used.
+
+    kwargs: dict, default None
+        Dictionary with kwargs passed to matplotlib.lines.Line2D
+    '''
+    import matplotlib.lines as mlines
+
+    if ax is None:
+        ax = plt.gca()
+
+    keys = kwargs.keys()
+    if 'zorder' not in keys:
+        kwargs['zorder'] = 5
+
+    if 'lw' not in keys:
+        kwargs['lw'] = 2.
+
+    if 'color' not in keys:
+        kwargs['color'] = 'black'
+
+    means = data.groupby(x)[y].mean()
+    sd = data.groupby(x)[y].std()
+    pooled_sd = sd.mean()
+    lower_sd = means - sd
+    upper_sd = means + sd
+
+    medians = data.groupby(x)[y].median()
+    quantiles = data.groupby(x)[y].quantile([0.25, 0.75]).unstack()
+    lower_quartiles = quantiles[0.25]
+    upper_quartiles = quantiles[0.75]
+
+    if type == 'mean_sd':
+        central_measures = means
+        lows = lower_sd
+        highs = upper_sd
+    elif type == 'median_quartiles':
+        central_measures = medians
+        lows = lower_quartiles
+        highs = upper_quartiles
+
+    original_zorder = kwargs['zorder']
+    span_color = kwargs['color']
+    span_lw = kwargs['lw']
+    for xpos, cm in enumerate(central_measures):
+        # add vertical span line.
+        kwargs['zorder'] = original_zorder
+        kwargs['color'] = span_color
+        kwargs['lw'] = span_lw
+        low_to_high = mlines.Line2D([xpos+offset, xpos+offset],
+                                    [lows[xpos], highs[xpos]],
+                                      **kwargs)
+        ax.add_line(low_to_high)
+
+        # add horzontal central measure line.
+        kwargs['zorder'] = 6
+        kwargs['color'] = 'white'
+        kwargs['lw'] = 2
+        mean_line = mlines.Line2D([xpos+offset-0.01,
+                                    xpos+offset+0.01],
+                                    [cm, cm],
+                                    **kwargs)
+        ax.add_line(mean_line)
